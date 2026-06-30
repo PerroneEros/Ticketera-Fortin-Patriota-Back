91c8db5eca32bc6067a870155d41675e025559e1
brunofernandez87 <brunoemanuelfernandez1@gmail.com>
2026-06-30 19:33:14 -0300

    agregacion de productos eliminados y ruta de status de producto

--- ticketera-fortin/src/main/services/productService.ts
+++ ticketera-fortin/src/main/services/productService.ts
@@ -3,33 +3,43 @@ import Category from '../model/category'
 import { CreateProductInput, UpdateProductInput } from '../model/interface/productInputs'
 
 export const productService = {
-  
   // Lista todos los productos
   async getAllProducts() {
     return await Product.findAll({
       // FILTRO: Solo traemos los que tengan isActive en true. Los "borrados" se ignoran.
-      where: { isActive: true }, 
-      
+      where: { isActive: true },
+
       // Le decimos a Sequelize que cruce los datos con la tabla Category y nos traiga solamente la columna 'name' de la categoría para no cargar datos inútiles.
       include: [{ model: Category, attributes: ['name'] }],
-      
+
       // ORDEN: Ordenamos los productos por la columna 'name' de manera ascendente (A-Z)
       order: [['name', 'ASC']]
-    }) 
+    })
   },
+  async getAllProductsDisable() {
+    return await Product.findAll({
+      // FILTRO: Solo traemos los que tengan isActive en true. Los "borrados" se ignoran.
+      where: { isActive: false },
 
-  // Busca un producto 
+      // Le decimos a Sequelize que cruce los datos con la tabla Category y nos traiga solamente la columna 'name' de la categoría para no cargar datos inútiles.
+      include: [{ model: Category, attributes: ['name'] }],
+
+      // ORDEN: Ordenamos los productos por la columna 'name' de manera ascendente (A-Z)
+      order: [['name', 'ASC']]
+    })
+  },
+  // Busca un producto
   async getProductById(id: string) {
     // findOne nos permite buscar por ID, pero agregando más condiciones (como isActive)
     const product = await Product.findOne({
-      where: { id_product: id, isActive: true }, 
+      where: { id_product: id, isActive: true },
       include: [{ model: Category, attributes: ['name'] }]
     })
-    
+
     // Si no existe, o si existe pero su isActive es false, tiramos error.
     // Este error será atrapado por el bloque 'catch' del controlador.
     if (!product) throw new Error('Producto no encontrado.')
-    
+
     return product
   },
 
@@ -43,13 +53,13 @@ export const productService = {
 
     // 2. BUSCAMOS SI EL NOMBRE YA EXISTE (Activo o Inactivo)
     const productExists = await Product.findOne({ where: { name: data.name } })
-    
+
     if (productExists) {
       // Si el producto ya existe y está activo, frenamos la creación.
       if (productExists.isActive) {
         throw new Error('Ya existe un producto activo con este nombre.')
-      } 
-      
+      }
+
       // Si existe pero está inactivo, lo reactivamos y le pisamos el precio y la categoría con los datos nuevos.
       return await productExists.update({
         price: data.price,
@@ -57,9 +67,9 @@ export const productService = {
         isActive: true // Lo volvemos a activar
       })
     }
-    
+
     // 3. Si la categoría existe y el nombre no se repite en absoluto, creamos el producto nuevo
-    return await Product.create(data as any) 
+    return await Product.create(data as any)
   },
 
   // Actualiza datos de un producto
@@ -73,7 +83,7 @@ export const productService = {
       throw new Error('No puedes modificar un producto que está deshabilitado. Reactívalo primero.')
     }
 
-    // 3. Si dentro de los datos a actualizar viene un cambio de categoría, 
+    // 3. Si dentro de los datos a actualizar viene un cambio de categoría,
     // debemos verificar nuevamente que esa NUEVA categoría exista en la base de datos.
     if (data.category_id) {
       const categoryExists = await Category.findByPk(data.category_id)
@@ -84,12 +94,12 @@ export const productService = {
     // Si envían un nombre nuevo, verificamos que no choque con otro producto distinto
     if (data.name && data.name !== product.name) {
       const nameInUse = await Product.findOne({ where: { name: data.name } })
-      
+
       if (nameInUse) {
         // Le damos un mensaje detallado dependiendo de si el otro producto está activo o inactivo
         throw new Error(
-          nameInUse.isActive 
-            ? 'El nombre ingresado ya pertenece a otro producto activo.' 
+          nameInUse.isActive
+            ? 'El nombre ingresado ya pertenece a otro producto activo.'
             : 'El nombre ingresado pertenece a un producto deshabilitado. No puedes usar este nombre.'
         )
       }
@@ -104,11 +114,11 @@ export const productService = {
     // 1. Buscamos el producto
     const product = await Product.findByPk(id)
     if (!product) throw new Error('Producto no encontrado.')
-    
-    // 2. BORRADO LÓGICO: 
+
+    // 2. BORRADO LÓGICO:
     // En lugar de destruirlo con product.destroy(), simplemente actualizamos su estado a false.
     await product.update({ isActive: false })
-    
+
     return true
   },
 
@@ -116,13 +126,13 @@ export const productService = {
   async reactivateProduct(id: string) {
     // Buscamos el producto sin importar si está activo o inactivo
     const product = await Product.findByPk(id)
-    
+
     if (!product) throw new Error('Producto no encontrado.')
     if (product.isActive) throw new Error('Este producto ya se encuentra activo.')
-    
+
     // Le devolvemos el estado activo
     await product.update({ isActive: true })
-    
+
     return true
   }
 }
