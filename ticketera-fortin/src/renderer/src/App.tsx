1bbf7af1b88cce011770ad0c1100b77be427a5b9
brunofernandez87 <brunoemanuelfernandez1@gmail.com>
2026-06-30 20:53:46 -0300

    correccion de products y app

--- ticketera-fortin/src/renderer/src/App.tsx
+++ ticketera-fortin/src/renderer/src/App.tsx
@@ -1,7 +1,7 @@
 import logo from './assets/logo.jpeg'
 import Header from './components/header/header'
 import { ProductListProvider } from './components/context/productListContext'
-import { CategoryProvider } from './components/context/categoryContext' 
+import { CategoryProvider } from './components/context/categoryContext'
 import Routs from './routes'
 import { ProductListProviderDisable } from './components/context/productListDisableContext'
 
@@ -11,20 +11,13 @@ function App(): React.JSX.Element {
   return (
     <>
       <Header />
-<<<<<<< HEAD
-      <ProductListProvider>
-        <CategoryProvider>
-          <Routs />
-        </CategoryProvider>
-      </ProductListProvider>
-=======
-      <ProductListProviderDisable>
-        <ProductListProvider>
-          <Routs />
-        </ProductListProvider>
-      </ProductListProviderDisable>
-      <Versions></Versions>
->>>>>>> 4da53afb523a4098c2c3e248900de59a3966623a
+      <CategoryProvider>
+        <ProductListProviderDisable>
+          <ProductListProvider>
+            <Routs />
+          </ProductListProvider>
+        </ProductListProviderDisable>
+      </CategoryProvider>
     </>
   )
 }
