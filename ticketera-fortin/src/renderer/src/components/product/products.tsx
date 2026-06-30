39747fac197b2b954368d9b76f967416efd207e0
brunofernandez87 <brunoemanuelfernandez1@gmail.com>
2026-06-30 20:58:27 -0300

    arreglo de products

--- ticketera-fortin/src/renderer/src/components/product/products.tsx
+++ ticketera-fortin/src/renderer/src/components/product/products.tsx
@@ -1,7 +1,17 @@
-import CardProducts from './cardProducts'
+import React from 'react'
+import CardProducts from '../product/cardProducts'
+import { CategoryFilter } from '../category/categoryFilter'
+import { CategoryActions } from '../category/categoryActions'
+
 export default function Products() {
   return (
     <div className="home-container">
+      <div className="toolbar-container">
+        <CategoryActions />
+      </div>
+      <div className="filters-container">
+        <CategoryFilter />
+      </div>
       <div className="Card-ProductsHome">
         <CardProducts />
       </div>
