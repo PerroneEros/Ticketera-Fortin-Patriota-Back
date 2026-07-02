a73d79987853f7f35ae46e790a66417b191819a6
brunofernandez87 <brunoemanuelfernandez1@gmail.com>
2026-07-02 19:47:41 -0300

    arreglos del merge

--- ticketera-fortin/src/renderer/src/components/header/header.tsx
+++ ticketera-fortin/src/renderer/src/components/header/header.tsx
@@ -1,12 +1,9 @@
 import { Link, useLocation } from 'react-router-dom'
 import logo from '../../assets/logo.jpeg'
-<<<<<<< HEAD
-=======
 import { CashRegisterClose } from '../cashRegister/cashRegsiterClose'
->>>>>>> 1b67bf4e9f959399633b6f09f4d86957efde9803
 
 export default function Header() {
-  const location = useLocation() 
+  const location = useLocation()
   if (location.pathname === '/') {
     return null
   }
@@ -33,18 +30,11 @@ export default function Header() {
           <button>Ver productos</button>
         </Link>
 
-<<<<<<< HEAD
         <Link to="/resumen" title="Resumen">
           <button>Resumen</button>
         </Link>
-        
         {/* Agregar un if que si no esta abierta la caja solo se vea resumen */}
-        <button>cerrar caja</button>
-=======
-        <button>Resumen</button>
-        
         <CashRegisterClose />
->>>>>>> 1b67bf4e9f959399633b6f09f4d86957efde9803
       </nav>
     </header>
   )
--- ticketera-fortin/src/renderer/src/routes.tsx
+++ ticketera-fortin/src/renderer/src/routes.tsx
@@ -1,23 +1,9 @@
 import { Route, Routes } from 'react-router-dom'
-// IMPORTAMOS HOME AQUÍ (Verifica que la ruta coincida con donde guardaste home.tsx)
-import Home from './components/home' 
+import Home from './components/home'
 import Products from './components/product/products'
 import ProductsList from './components/product/productsList'
 import ActivateProducts from './components/product/activateProduct'
-<<<<<<< HEAD
 import { DashboardView } from './components/dashboard/DashboardView'
-export default function Routs() {
-  return (
-    <>
-      <Routes>
-        <Route path="/products" element={<Products />} />
-        <Route path="/list-products" element={<ProductsList />} />
-        <Route path="/activate-products" element={<ActivateProducts />} />
-        <Route path="/resumen" element={<DashboardView />} />
-      </Routes>
-    </>
-=======
-
 export default function Routs() {
   return (
     <Routes>
@@ -25,7 +11,7 @@ export default function Routs() {
       <Route path="/products" element={<Products />} />
       <Route path="/list-products" element={<ProductsList />} />
       <Route path="/activate-products" element={<ActivateProducts />} />
+      <Route path="/resumen" element={<DashboardView />} />
     </Routes>
->>>>>>> 1b67bf4e9f959399633b6f09f4d86957efde9803
   )
 }
