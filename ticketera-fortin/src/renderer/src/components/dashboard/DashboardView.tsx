cebb01283187a4b456649bcf25f42caa6d939070
PerroneEros <erosperr@gmail.com>
2026-07-04 15:18:47 -0300

    creacion de historial

--- ticketera-fortin/src/renderer/src/components/dashboard/DashboardView.tsx
+++ ticketera-fortin/src/renderer/src/components/dashboard/DashboardView.tsx
@@ -3,27 +3,49 @@ import { DashboardHeader } from './DashboardHeader'
 import { DashboardCards } from './DashboardCards'
 import { DashboardPayments } from './DashboardPayments'
 import { DashboardProducts } from './DashboardProducts'
+import { DashboardHistory } from './DashboardHistory'
 import { DashboardProvider } from '../context/dashboardContext'
 
 export const DashboardView = () => {
   return (
-    // Envolvemos todo en el Provider para que escuche los filtros de tiempo
     <DashboardProvider>
-      <div className="dashboard-container" style={{ padding: '20px', gap: '20px', display: 'flex', flexDirection: 'column' }}>
+      
+      <div style={{ 
+        width: '100%', 
+        height: 'calc(100vh - 180px)', 
+        overflowY: 'auto',
+        paddingTop: '80px' 
+      }}>
+
+        <div className="dashboard-container" style={{ 
+          padding: '20px', 
+          gap: '20px', 
+          display: 'flex', 
+          flexDirection: 'column',
+          maxWidth: '900px', 
+          margin: '0 auto',   
+          paddingBottom: '40px'
+        }}>
+
+          {/*Botonera de arriba (Día, Semana, Mes y Movimiento) */}
+          <DashboardHeader />
+
+          {/*Tarjetitas de Resumen */}
+          <DashboardCards />
+
+          {/* Barras de Efectivo y Transferencia */}
+          <DashboardPayments />
+
+          {/*Lista de productos más vendidos */}
+          <DashboardProducts />
+
+          {/*Historial*/}
+          <DashboardHistory />
+          
+        </div>
         
-        {/*Botonera de arriba (Día, Semana, Mes y Movimiento) */}
-        <DashboardHeader />
-
-        {/*Tarjetitas de Resumen */}
-        <DashboardCards />
-
-        {/* Barras de Efectivo y Transferencia */}
-        <DashboardPayments />
-
-        {/*Lista de productos más vendidos */}
-        <DashboardProducts />
-
       </div>
+
     </DashboardProvider>
   )
 }
