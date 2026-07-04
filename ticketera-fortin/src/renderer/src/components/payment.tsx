a1eab0d56aa09ef45c2c1651b3c567dbb259e309
brunofernandez87 <brunoemanuelfernandez1@gmail.com>
2026-07-04 15:38:00 -0300

    funcion de payment

---
+++ ticketera-fortin/src/renderer/src/components/payment.tsx
@@ -0,0 +1,63 @@
+import { useState } from 'react'
+
+export default function Payment({ method, total, onClose, onConfirm }) {
+  const [cash, setCash] = useState(0)
+  const [transfer, setTransfer] = useState(0)
+  const handlePayment = async (e) => {
+    e.preventDefault()
+    onConfirm({
+      cashAmount: cash,
+      transferAmount: transfer
+    })
+  }
+  return (
+    <div className="modal-overlay">
+      <div className="modal-content">
+        <div>
+          <h3>Ingrese el monto (Total: ${total})</h3>
+          <button className="btn-prod-close" onClick={onClose}>
+            ✕
+          </button>
+        </div>
+        <form onSubmit={handlePayment}>
+          <div>
+            <div>
+              {(method == 'efectivo' || method == 'combinado') && (
+                <>
+                  <label>Efectivo:</label>
+                  <input
+                    type="number"
+                    onChange={(c) => {
+                      setCash(Number(c.target.value))
+                    }}
+                  />
+                </>
+              )}
+            </div>
+            <div>
+              {(method == 'transferencia' || method == 'combinado') && (
+                <>
+                  <label>Transferencia:</label>
+                  <input
+                    type="number"
+                    onChange={(t) => {
+                      setTransfer(Number(t.target.value))
+                    }}
+                  />
+                </>
+              )}
+            </div>
+            <div>
+              <button type="submit">Confirmar</button>
+            </div>
+            <div>
+              <button type="button" onClick={onClose}>
+                Cancelar
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
