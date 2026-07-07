28e77f9aa9ee53703ccfe66ac9a7beb3c32abed4
Ivo Depari <ivodepari1302@hotmail.com>
2026-07-07 14:39:06 -0300

    EStilos de windowConfirm y modificacion para resaltar texto

--- ticketera-fortin/src/renderer/src/components/windowConfirm.tsx
+++ ticketera-fortin/src/renderer/src/components/windowConfirm.tsx
@@ -1,18 +1,25 @@
-import './Styles/productModals.css'
+import './Styles/ConfirmModals.css'
 export default function WindowConfirm({ text, onConfirm, onClose }) {
   return (
     <div className="modal-prod-overlay">
-      <div className="modal-prod-content">
-        <div className="modal-prod-header">
-          <p>Esta seguro de {text}?</p>
+      <div className="confirm-modal-content">
+
+        <div className="confirm-modal-header">
+          <h3>Confirmación</h3>
           <button className="btn-prod-close" onClick={onClose}>
             ✕
           </button>
         </div>
-        <div className="modal-prod-actions">
-          <button onClick={onConfirm}>Confirmar</button>
-          <button onClick={onClose}>Cancelar</button>
+
+        <div className="confirm-modal-body">
+          <p>¿Está seguro de <b>{text}</b>?</p>
         </div>
+
+        <div className="confirm-modal-actions">
+          <button className="btn-cancel-confirm" onClick={onClose}>Cancelar</button>
+          <button className="btn-accept-confirm" onClick={onConfirm}>Confirmar</button>
+        </div>
+
       </div>
     </div>
   )
